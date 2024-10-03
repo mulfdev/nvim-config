58f7a3d735321c4e4d4d4d76a038a575b6daa273
mulf <mail@mulf.wtf>
2024-10-03 14:26:43 -0500

    clear it

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -10,18 +10,6 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
   },
   {
-    'prettier/vim-prettier',
-    run = 'yarn install',
-    config = function()
-      vim.g['prettier#config#config_precedence'] = 'prefer-file'
-      vim.g['prettier#exec_cmd_path'] = 'prettier'
-      vim.g['prettier#autoformat'] = 1
-      vim.g['prettier#autoformat_require_pragma'] = 0
-      vim.g['prettier#quickfix_enabled'] = 1
-    end,
-  },
-  { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
-  {
     'sho-87/kanagawa-paper.nvim',
     lazy = false,
     priority = 1000,

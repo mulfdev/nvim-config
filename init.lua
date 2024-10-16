12bdaa937d568321bfef011af8ffcbbdc8208f4c
mulf <mail@mulf.wtf>
2024-10-16 16:23:46 -0500

    fuzzy

--- init.lua
+++ init.lua
@@ -417,7 +417,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
 
       -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
+      vim.keymap.set('n', '/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
         builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
           winblend = 10,
